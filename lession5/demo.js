c490b99cb8ce8112c4f690f76846ceadce1e92db
Vu Phuc Quang Vinh <vinhvpq2004@gmail.com>
2025-11-19 10:36:47 +0700

    Refactor demo files: update HTML structure and enhance JavaScript functionality for dynamic element creation

--- lession5/demo.js
+++ lession5/demo.js
@@ -1,2 +1,28 @@
-const p = document.getElementsByTagName('p');
-console.log(p);
+// const p = document.getElementsByTagName('p');
+// console.log(p);
+
+// const parent = document.getElementById('container');
+// console.log(parent);
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+// console.log(parent.firstElementChild.nextElementSibling);
+// console.log(parent.children);
+
+// const countChild = parnent.childElementCount;
+// console.log('So luong con: ' + countChild);
+
+
+
+const parent = document.getElementById('container');
+// them moi su kien click
+const create = document.getElementById('add');
+const remove = document.getElementById('remove');
+create.addEventListener('click', function () {
+    // cach 1 : tao phan tu tu phuong thuc createElement
+    const child1 = document.createElement('h2');
+    child1.innerText = 'Day la the h2 duoc them vao';
+    // them phan tu vao parent
+    parent.appendChild(child1);
+    // cach 2: tao phan tu tu thuoc tinh innerHTML
+    parent.innerHTML += '<h3>Day la the h3 duoc them vao</h3>';
+});
